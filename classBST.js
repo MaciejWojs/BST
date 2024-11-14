689978a88c8088f10817e32dace93c272b00a0b7
ar00k <ar00k@users.noreply.github.com>
2024-11-14 21:14:40 +0000

    deploy: d90d9383d0164a4701fcb8f19f84182dc633c710

--- classBST.js
+++ classBST.js
@@ -5,6 +5,8 @@ var classBST =
     [ "add_node", "classBST.html#a38530a88cc3f38e582413742f1579414", null ],
     [ "delete_node", "classBST.html#ababcb2cdc6743b8de0026804337f0231", null ],
     [ "display", "classBST.html#abacd7ba4914718e5db56257a57616554", null ],
+    [ "display_postorder", "classBST.html#a7491aff095c65f91a9e28b9f970cc2e8", null ],
+    [ "display_preorder", "classBST.html#ae527ac1b2d63b60ff1224dacbc494dee", null ],
     [ "purge", "classBST.html#ab57605c2f6cbe553f00885a8e29525c0", null ],
     [ "save", "classBST.html#adc7b5abe38090a2d813c1dce3a5a1979", null ]
 ];
