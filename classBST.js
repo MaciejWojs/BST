1ffae06c35c867b49c6a572813ee946599211eed
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-11-20 18:55:21 +0000

    deploy: a55c8acf5f90bb12225259aacd6019c0011741d8

--- classBST.js
+++ classBST.js
@@ -3,7 +3,7 @@ var classBST =
     [ "BST", "classBST.html#abc17123a0367c3b8ad0382eeb3ad3178", null ],
     [ "~BST", "classBST.html#aff9c7948fbba37844d2893b920ddc238", null ],
     [ "add_node", "classBST.html#a38530a88cc3f38e582413742f1579414", null ],
-    [ "delete_node", "classBST.html#ababcb2cdc6743b8de0026804337f0231", null ],
+    [ "delete_node", "classBST.html#a4f27aab9ac30f53a07c0a4d038b60622", null ],
     [ "display", "classBST.html#abacd7ba4914718e5db56257a57616554", null ],
     [ "display_tree", "classBST.html#af1a1696725c0747b07f23d99c6d01bd8", null ],
     [ "find", "classBST.html#aa507fc9358d1dd2bef9f684b79e0fd26", null ],
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['delete_5fnode_0',['delete_node',['../classBST.html#ababcb2cdc6743b8de0026804337f0231',1,'BST']]],
+  ['delete_5fnode_0',['delete_node',['../classBST.html#a4f27aab9ac30f53a07c0a4d038b60622',1,'BST']]],
   ['display_1',['display',['../classBST.html#abacd7ba4914718e5db56257a57616554',1,'BST']]],
   ['display_5ftree_2',['display_tree',['../classBST.html#af1a1696725c0747b07f23d99c6d01bd8',1,'BST']]]
 ];
